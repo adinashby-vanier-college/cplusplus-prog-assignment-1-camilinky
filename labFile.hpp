a8d0a58d3acca94467a1bb649a180a75474a36cd
camilinky <158082505+camilinky@users.noreply.github.com>
2024-03-04 15:22:19 -0500

    Update labFile.hpp

--- labFile.hpp
+++ labFile.hpp
@@ -4,25 +4,82 @@
 #define LABFILE_HPP
 
 int sum(int a, int b) {
-    return 0;
+
+    int sum = a + b;
+
+    return sum;
+        
 }
 
 char findGrade(int score) {
+
+    char findGrade(int score); {
+        if (score >= 90) {
+            return 'A';
+        }
+        else if (score >= 80) {
+            return 'B';
+        }
+        else if (score >= 70) {
+            return 'C';
+        }
+        else if (score >= 60) {
+            return 'D';
+        }
+        else {
+            return 'F';
+        }
+    }
+
+
     return '.';
 }
 
 bool isVowel(char ch) {
+
+    ch = tolower(ch);
+    bool isVowel(char ch); {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        else {
+            return false;
+        }
+    }
+
     return '.';
 }
 
 bool isLeapYear(int year) {
-    return false;
+
+    bool isLeapYear = false;
+    if (year % 4 == 0) {
+        isLeapYear = true;
+    }
+    if (year % 100 == 0) {
+        isLeapYear = false;
+    }
+    if (year % 400 == 0) {
+        isLeapYear = true;
+    }
+    return isLeapYear;
 }
 
 int max(int a, int b, int c) {
+    if (c < a && b < a) {
+        return a;
+    }
+    if (a < c && b < c) {
+        return c;
+    }
+    if (a < b && c < b) {
+        return b;
+    }
+    if (a == b && b == c) {
+        return a;
+    }
+
+
     return 0;
 }
-
-
 
 #endif
